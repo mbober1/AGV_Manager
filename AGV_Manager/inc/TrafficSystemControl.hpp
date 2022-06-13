bef9c40504908a182e4093bff8fe0bfbabccc7b6
spror <wiktorspringer27@gmail.com>
2022-06-13 14:56:10 +0200

    add a small changes

--- AGV_Manager/inc/TrafficSystemControl.hpp
+++ AGV_Manager/inc/TrafficSystemControl.hpp
@@ -15,6 +15,7 @@ enum class Point_State
 using status_points = std::vector<std::pair< Point_State, int>>;
 using shared_path_points =std::vector<std::vector<std::list<int>>>;
 using AGVs = std::shared_ptr<std::vector<AGV>>;
+using road = std::vector<std::list<int>>;
 
 /*
  * Class to handle collision protection. Created according to:
@@ -27,7 +28,7 @@ class TrafficSystemControl{
 status_points points_with_status;
 AGVs AGVs_vector;
 shared_path_points shared_points_vector;
-std::vector<std::list<int>> paths;
+road paths;
 
 void clear_shared_points();
 bool find_element(int key, std::list<int> container);
@@ -36,6 +37,7 @@ bool condition_2(int point, int agv_id, int current_point);
 bool reserved_point_in_shared_set(std::list<int> container,  int current);
 
 public:
+    TrafficSystemControl() = delete;
     TrafficSystemControl(Warehouse  &warehouse, AGVs AGVs_vector);
 
 
